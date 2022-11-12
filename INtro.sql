c918dfeff5a446111defe4d1f4d7c03ceba7129a
Victory567 <110156761+Victory567@users.noreply.github.com>
2022-11-12 16:28:28 +0100

    Add files via upload
    SQL INTRO

---
+++ INtro.sql
@@ -0,0 +1,12 @@
+use sakila;
+
+select * from  actor;
+select * from film;
+select * from customer;
+select * from film_list;
+
+select count(*) from store;
+select count(*) from staff;
+select * from staff group by first_name 
+
+
